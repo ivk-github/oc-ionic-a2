3efc6f885a5ff39e071d230fc0c92716c54448c0
Ivan Katjounis <ivk.msg+github@gmail.com>
2019-08-02 22:10:29 +0200

    012. Create BooksListPage

--- src/app/pages/books-list/books-list.page.ts
+++ src/app/pages/books-list/books-list.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { BookModel } from 'src/app/models/book-model';
+
+import { MediasService } from 'src/app/services/medias.service';
 
 @Component({
   selector: 'app-books-list',
   templateUrl: './books-list.page.html',
   styleUrls: ['./books-list.page.scss'],
 })
-export class BooksListPage implements OnInit {
+export class BooksListPage implements OnInit, OnDestroy {
 
-  constructor() { }
+  booksSubscription: Subscription;
+  booksList: BookModel[];
+
+  constructor(private mediasService: MediasService) { }
 
   ngOnInit() {
+    this.booksSubscription = this.mediasService.booksList$.subscribe(
+      (booksList: BookModel[]) => {
+        this.booksList = booksList.slice();
+      }
+    );
+    this.mediasService.emitBooks();
   }
 
+  ngOnDestroy() {
+    this.booksSubscription.unsubscribe();
+  }
+
+  async onDisplayLendBook(bookId: number) {
+    console.log("onDisplayLendBook - bookId : " + bookId);
+  }
 }
