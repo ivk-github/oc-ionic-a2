19c998604b5b532f6102272535d3467d5b157bf8
Ivan Katjounis <ivk.msg+github@gmail.com>
2019-08-02 16:14:43 +0200

    006. Create empty DisksListPage

---
+++ src/app/pages/disks-list/disks-list.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disks-list',
+  templateUrl: './disks-list.page.html',
+  styleUrls: ['./disks-list.page.scss'],
+})
+export class DisksListPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
