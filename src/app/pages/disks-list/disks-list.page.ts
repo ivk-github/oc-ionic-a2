bfcd6f49b2e29f18e9abbd8d6d6328f6f8ba3ef5
Ivan Katjounis <ivk.msg+github@gmail.com>
2019-08-02 22:24:36 +0200

    013. Create DisksListPage

--- src/app/pages/disks-list/disks-list.page.ts
+++ src/app/pages/disks-list/disks-list.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { DiskModel } from 'src/app/models/disk-model';
+
+import { MediasService } from 'src/app/services/medias.service';
 
 @Component({
   selector: 'app-disks-list',
   templateUrl: './disks-list.page.html',
   styleUrls: ['./disks-list.page.scss'],
 })
-export class DisksListPage implements OnInit {
+export class DisksListPage implements OnInit, OnDestroy {
 
-  constructor() { }
+  disksSubscription: Subscription;
+  disksList: DiskModel[];
+
+  constructor(private mediasService: MediasService) { }
 
   ngOnInit() {
+    this.disksSubscription = this.mediasService.disksList$.subscribe(
+      (disksList: DiskModel[]) => {
+        this.disksList = disksList.slice();
+      }
+    );
+    this.mediasService.emitDisks();
   }
 
+  ngOnDestroy() {
+    this.disksSubscription.unsubscribe();
+  }
+
+  async onDisplayLendDisk(diskId: number) {
+    console.log("onDisplayLendDisk - diskId : " + diskId);
+  }
 }
