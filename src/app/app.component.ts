02a763771bc41cd7f172394e759bc8b62b5345a5
Ivan Katjounis <ivk.msg+github@gmail.com>
2019-08-07 10:56:22 +0200

    Firebase integration to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import * as firebase from 'firebase';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -22,6 +24,17 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      let firebaseConfig = {
+        apiKey: "AIzaSyBCrh4NB3sHQcFrodZiV5liB5cOoyfoqHY",
+        authDomain: "ivk-oc-ionic-a2.firebaseapp.com",
+        databaseURL: "https://ivk-oc-ionic-a2.firebaseio.com",
+        projectId: "ivk-oc-ionic-a2",
+        storageBucket: "",
+        messagingSenderId: "389126851148",
+        appId: "1:389126851148:web:563508f42c9722e4"
+      };
+      firebase.initializeApp(firebaseConfig);
     });
   }
 }
